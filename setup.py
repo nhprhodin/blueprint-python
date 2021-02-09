8fcf45d714033d4c0b0c554452c97b4a0d81031e
nhprhodin <nhp.rhodin@gmail.com>
2021-02-10 00:09:40 +0100

    Added setup.py for package creation.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(name='blueprint-python-nhprhodin',
+      version='0.0.1',
+      description='A Python-project blueprint.',
+      long_description=readme,
+      url='https://github.com/nhprhodin/blueprint-python',
+      author='Henrik P. Rhodin',
+      author_email='nhp.rhodin@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      test_suite='tests',
+      classifiers=[
+              "Programming Language :: Python :: 3",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: POSIX :: Linux"
+      ],
+      python_requires='>=3.7',
+      include_package_data=True,
+      data_files=['requirements.txt'],
+      zip_safe=False
+      )
