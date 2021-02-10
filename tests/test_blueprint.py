60aab1f1064c983b3628f81102739eeb09cb1528
nhprhodin <nhp.rhodin@gmail.com>
2021-02-10 23:21:05 +0100

    Added unit tests and tox configuration for code quality assessment.

---
+++ tests/test_blueprint.py
@@ -0,0 +1,41 @@
+import unittest
+
+from blueprint.blueprint import Blueprint
+
+
+class BlueprintTest(unittest.TestCase):
+    def test_multiply_ints_signs(self):
+        """
+        Test that multiply_ints correctly accounts for the sign of the arguments.
+        """
+
+        result = Blueprint.multiply_ints(1, 1)
+        self.assertEqual(result, 1)
+
+        result = Blueprint.multiply_ints(-1, 1)
+        self.assertEqual(result, -1)
+
+        result = Blueprint.multiply_ints(-1, -1)
+        self.assertEqual(result, 1)
+
+        result = Blueprint.multiply_ints(0, 0)
+        self.assertEqual(result, 0)
+
+    def test_multiply_ints_types(self):
+        """
+        Test that multiply_ints raises TypeError if an input argument is not of type int.
+        """
+
+        int_input = int(10)
+
+        string_input = "not_a_number"
+        self.assertRaises(TypeError, Blueprint.multiply_ints, int_input, string_input)
+
+        float_input = float(7)
+        self.assertRaises(TypeError, Blueprint.multiply_ints, int_input, float_input)
+
+        dict_input = {}
+        self.assertRaises(TypeError, Blueprint.multiply_ints, int_input, dict_input)
+
+        list_input = []
+        self.assertRaises(TypeError, Blueprint.multiply_ints, int_input, list_input)
