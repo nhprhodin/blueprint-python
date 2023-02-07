29187c8a660f30f81c9b9971640a71d5c0e4d802
nhprhodin <nhp.rhodin@gmail.com>
2023-02-07 22:04:09 +0100

    Generalized example method to consider a union of float and int.

--- blueprint/blueprint.py
+++ blueprint/blueprint.py
@@ -16,7 +16,13 @@ class Blueprint:
         :param xx: Multiplier
         :param yy: Multiplicand
         :return: Product of multiplication.
+
+        TODO: Can Union[] be used to simplify TypeError catch?
+        if not isinstance(xx, Union[float, int]) or not isinstance(yy, Union[float, int]):
+            raise TypeError("Input arguments must be of type float or int.")
+
         """
-        if not isinstance(xx, int) or not isinstance(yy, int):
-            raise TypeError("Input arguments must be of type integer.")
+        if (not isinstance(xx, float) and not isinstance(xx, int)) or (not isinstance(yy, float) and not
+           isinstance(yy, int)):
+            raise TypeError("Input arguments must be of type float or int.")
         return float(xx * yy)
--- tests/test_blueprint.py
+++ tests/test_blueprint.py
@@ -4,38 +4,39 @@ from blueprint.blueprint import Blueprint
 
 
 class BlueprintTest(unittest.TestCase):
-    def test_multiply_ints_signs(self):
+    def test_multiply_numbers_signs(self):
         """
-        Test that multiply_ints correctly accounts for the signs of the arguments.
-        """
-
-        result = Blueprint.multiply_ints(1, 1)
-        self.assertEqual(result, 1)
-
-        result = Blueprint.multiply_ints(-1, 1)
-        self.assertEqual(result, -1)
-
-        result = Blueprint.multiply_ints(-1, -1)
-        self.assertEqual(result, 1)
-
-        result = Blueprint.multiply_ints(0, 0)
-        self.assertEqual(result, 0)
-
-    def test_multiply_ints_types(self):
-        """
-        Test that multiply_ints raises TypeError if an input argument is not of type int.
+        Test that multiply_numbers correctly accounts for the signs of the arguments.
         """
 
-        int_input = int(10)
+        result = Blueprint.multiply_numbers(1, 1)
+        self.assertEqual(result, float(1))
+
+        result = Blueprint.multiply_numbers(-1, 1)
+        self.assertEqual(result, float(-1))
+
+        result = Blueprint.multiply_numbers(-1, -1)
+        self.assertEqual(result, float(1))
+
+        result = Blueprint.multiply_numbers(0, 0)
+        self.assertEqual(result, float(0))
+
+    def test_multiply_numbers_types(self):
+        """
+        Test that multiply_numbers raises TypeError if an input argument is not of type int or float.
+        """
+
+        int_input = int(1)
+        float_input = float(1)
 
         string_input = "not_a_number"
-        self.assertRaises(TypeError, Blueprint.multiply_ints, int_input, string_input)
-
-        float_input = float(10)
-        self.assertRaises(TypeError, Blueprint.multiply_ints, int_input, float_input)
+        self.assertRaises(TypeError, Blueprint.multiply_numbers, int_input, string_input)
+        self.assertRaises(TypeError, Blueprint.multiply_numbers, float_input, string_input)
 
         dict_input = {}
-        self.assertRaises(TypeError, Blueprint.multiply_ints, int_input, dict_input)
+        self.assertRaises(TypeError, Blueprint.multiply_numbers, int_input, dict_input)
+        self.assertRaises(TypeError, Blueprint.multiply_numbers, float_input, dict_input)
 
         list_input = []
-        self.assertRaises(TypeError, Blueprint.multiply_ints, int_input, list_input)
+        self.assertRaises(TypeError, Blueprint.multiply_numbers, int_input, list_input)
+        self.assertRaises(TypeError, Blueprint.multiply_numbers, float_input, list_input)
